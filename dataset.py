767a9488ad9cab8ea248968478bc2e49b1cf2f1e
Ironbell <isabelleroesch@msn.com>
2017-03-12 18:46:04 +0100

    intermediate

--- dataset.py
+++ dataset.py
@@ -3,12 +3,23 @@
     Maybe augment?
 """
 import os, math, re
+import decimal
 import numpy as np
 import scipy as sp
 from sklearn.preprocessing import MinMaxScaler
 from attrdict import AttrDict
 
 from eccodes import *
+
+GRID_SIZE = 0.75
+
+def drange(x, y, jump):
+  while x < y:
+    yield float(x)
+    x += decimal.Decimal(jump)
+    
+def round_nearest(x, a):
+    return round(round(x / a) * a, -int(math.floor(math.log10(a))))
 
 def crop_center(img, width, height):
     """
@@ -21,7 +32,7 @@ def crop_center(img, width, height):
     starty = y//2-(height//2)    
     return img[starty:starty + height, startx:startx + width, :]
     
-def lonlat_to_idx(lon, lat, grid_lon=0.75, grid_lat=0.75):
+def lonlat_to_idx(lon, lat, grid_lon=GRID_SIZE, grid_lat=GRID_SIZE):
     """ 
         Converts longitude and latitude to the corresponding 
         indexes in a data matrix, given the grid density
@@ -31,7 +42,6 @@ def lonlat_to_idx(lon, lat, grid_lon=0.75, grid_lat=0.75):
 class DatasetSingle:
     """
         Loads data from a grid file and divides them into training and evaluation groups
-        The scalar is actually the one nearest Zurich
         @param window_size: how many frames should be considered for the forecast
         @param grib_file: the path to the grib file to load
         @param start_lon: the start longitude to consider for the data cropping
@@ -148,88 +158,145 @@ class DatasetSingle:
         print ("shape of test data Y is:")
         print (self.testY.shape)
 
-class Dataset2D:
+    
+class DatasetMultiple:
     """
-        Loads training images and divides them into training and evaluation groups
-        @param frame_count: how long is one "weather video"
+        Loads data from a grid file and divides them into training and evaluation groups
+        @param window_size: how many frames should be considered for the forecast
         @param grib_file: the path to the grib file to load
-        @param img_rows: how many rows in the final images (gets cropped)
-        @param img_cols: how many cols in the final images (gets cropped)
-        @param total_frames: maximum frames to load
+        @param start_lon: the start longitude to consider for the data cropping
+        @param end_lon: the end longitude to consider for the data cropping
+        @param start_lat: the start latitude to consider for the data cropping
+        @param end_lat: the end latitude to consider for the data cropping
     """
-    def __init__(self, grib_file, frame_count, img_rows, img_cols, total_frames):
-        self.grib_file = grib_file
-        self.load_frames(img_rows, img_cols, total_frames)
-        self.create_dataset(frame_count)
+    def __init__(self, params):
+        self.check_params(params)
+        self.load_frames(params)
+        self.create_sets(params.window_size, params.test_fraction)
+        
+    def check_params(self, params):
+        if params.test_fraction < 0 or params.test_fraction > 1:
+            raise Exception("test fraction must be between 0 and 1")
+            
+        if params.start_lat > params.end_lat:
+            raise Exception("latitude dimensions do not match")
+            
+        if params.start_lon > params.end_lon:
+            raise Exception("longitude dimensions do not match")  
 
-    def load_frames(self, img_rows, img_cols, total_frames):
+        if params.start_lon > 360 or params.start_lon < 0:
+            raise Exception("longitude (start) must be between 0 and 360")   
+            
+        if params.end_lon > 360 or params.end_lon < 0:
+            raise Exception("longitude (end) must be between 0 and 360")  
+            
+        if params.start_lat > 90 or params.start_lat < -90:
+            raise Exception("latitude (start) must be between -90 and 90")   
+            
+        if params.end_lat > 90 or params.end_lat < -90:
+            raise Exception("latitude (end) must be between -90 and 90") 
+            
+        if params.window_size < 1:
+            raise Exception("window size must be at least one") 
+         
+        if params.max_frames < params.window_size * 2:
+            raise Exception("max frames must be at least twice the window size")  
+            
+        self.vector_size = int(1 + ((round_nearest(params.end_lat, GRID_SIZE) - round_nearest(params.start_lat, GRID_SIZE)) / GRID_SIZE) * \
+            1 + ((round_nearest(params.end_lon, GRID_SIZE) - round_nearest(params.start_lon, GRID_SIZE)) / GRID_SIZE))
+            
+        print ("vector size is %i" % self.vector_size)
+        
+         
+    def load_frames(self, params):
         """ Loads the training files """
         self.frames = []
 
-        f = open(self.grib_file)
-        iter = 0
+        f = open(params.grib_file)
+        
+        index = 0
  
         while 1:
             gid = codes_grib_new_from_file(f)
             if gid is None:
                 break
-     
-            iterid = codes_grib_iterator_new(gid, 0)
-     
-            missingValue = codes_get_double(gid, "missingValue")
-
-            #for key in ('max', 'min', 'average'):
-            #    print ('%s=%.10e' % (key, codes_get(gid, key)))
                 
-            frame = np.empty([241,480,1], dtype=float)
-
-            while 1:
-                result = codes_grib_iterator_next(iterid)
-                if not result:
-                    break
-     
-                [lat, lon, value] = result
-                frame[lonlat_to_idx(lon, lat),:] = value
-     
-                if value == missingValue:
-                    print ("missing")
-
-
-            codes_grib_iterator_delete(iterid)
-            codes_release(gid)
-
-            # normalize?
-            # frame = frame.astype('float32')
-            #frame -= np.mean(frame)
-            #frame /= np.max(frame)
+            frame = np.empty([self.vector_size])
+            frameIt = 0
             
-            # cut... because, sadly it was killed with this size.
-            frame = crop_center(frame, img_cols, img_rows)     
+            topLeft = codes_grib_find_nearest(gid, params.start_lat, params.start_lon)[0]
+            bottomRight = codes_grib_find_nearest(gid, params.end_lat, params.end_lon)[0]
+            
+            for lat in drange(topLeft.lat, bottomRight.lat, 0.75):
+                for lon in drange(topLeft.lon, bottomRight.lon, 0.75):
+                    nearest = codes_grib_find_nearest(gid, lat, lon)[0]
+                    frame[frame_it] = max(0, nearest.value)
+                    if nearest.value == codes_get_double(gid, "missingValue"):
+                        print ("missing")
+                    frameIt = frameIt + 1
+                    
+            #print params.start_lat, params.start_lon
+            #print nearest.lat, nearest.lon, nearest.value, nearest.distance, \
+            #nearest.index
+
+            codes_release(gid)
             self.frames.append(frame)
             
-            iter += 1
-            if iter > total_frames:
+            index = index + 1
+            print "loading frames: ", index, "\r",
+            
+            if index >= params.max_frames:
                 break
-
+        
+        print ("")
+        self.frames = np.asarray(self.frames)
+        print ("frames shape")
+        print (self.frames.shape)
         f.close()
         
-    def create_dataset(self, window_size):
+    def create_dataset(self, dataset, window_size):
         """ convert an array of values into a dataset matrix """
-        dataX = []
-        dataY = []
-        i = 0
-        maxI = len(self.frames) - window_size - 2
-        while i < maxI:
-            dataX.append(self.frames[i:(i + window_size)])
-            dataY.append(self.frames[i + 1 :(i + window_size) + 1])
-            i += window_size
+        dataX, dataY = [], []
+        for i in range(len(dataset) - window_size - 1):
+            a = dataset[i:(i + window_size), :]
+            dataX.append(a)
+            dataY.append(dataset[i + window_size, :])
+        return np.array(dataX), np.array(dataY)
+    
+    def create_sets(self, window_size, test_fraction):
+        """ split data into training/test set and shape/format. """
+        # normalize the dataset
+        self.frames = self.frames.astype('float32')
         
-        self.dataX = np.array(dataX)
-        self.dataY = np.array(dataY)
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
+        self.frames = self.scaler.fit_transform(self.frames)
+        
+        # split into train and test sets
+        train_size = int(len(self.frames) * (1 - test_fraction))
+        test_size = len(self.frames) - train_size
+        print("loading %i training frames..." % train_size)
+        print("loading %i testing frames..." % test_size)
+        train, test = self.frames[0:train_size,:], self.frames[train_size:len(self.frames),:]
 
-        self.frames = [] # so that we don't duplicate the data
+        # reshape into X=t and Y=t+1
+        self.trainX, self.trainY = self.create_dataset(train, window_size)
+        self.testX, self.testY = self.create_dataset(test, window_size)
         
-        print ("shape of data X is:")
-        print (self.dataX.shape)
-        print ("shape of data Y is:")
-        print (self.dataY.shape)
+        # reshape input to be [samples, time steps, features]
+        #self.trainX = np.reshape(self.trainX, (self.trainX.shape[0], 1, self.trainX.shape[1]))
+        #self.testX = np.reshape(self.testX, (self.testX.shape[0], 1, self.testX.shape[1]))
+        
+        #self.trainX = np.random.rand(self.trainX.shape[0], 2, self.trainX.shape[1])
+        #self.testX = np.random.rand(self.testX.shape[0], 2, self.testX.shape[1])
+        #self.trainY = np.random.rand(self.trainY.shape[0], 2)
+        #self.testY = np.random.rand(self.testY.shape[0], 2)
+
+        print ("shape of train data X is:")
+        print (self.trainX.shape)
+        print ("shape of train data Y is:")
+        print (self.trainY.shape)
+        print ("shape of test data X is:")
+        print (self.testX.shape)
+        print ("shape of test data Y is:")
+        print (self.testY.shape)
+
---
+++ multiple_lstm_recurrent.py
@@ -0,0 +1,81 @@
+import numpy as np
+np.random.seed(123)  # for reproducibility
+
+import matplotlib.pyplot as plt
+import pandas
+import math
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from sklearn.metrics import mean_squared_error
+from attrdict import AttrDict
+
+from dataset import *
+ 
+#TRAINING_PATH = '/media/isa/VIS1/data/total_2mtemp.grib'
+TRAINING_PATH = 'soil_temp1_summer05.grib'
+
+def example():
+    window_size = 5
+    hidden_neurons = 100
+    in_out_neurons = 2
+    epoch_count = 10
+    params = AttrDict()
+    params.window_size = window_size
+    params.start_lat = 47.3769
+    params.end_lat = 47.3769
+    params.start_lon = 8.5417
+    params.end_lon = 8.5417
+    params.grib_file = TRAINING_PATH
+    params.max_frames = 1000
+    params.test_fraction = 0.3
+
+    # load the data from the .grib files
+    data = DatasetMultiple(params)
+    
+    # create and fit the LSTM network
+    model = Sequential()  
+    model.add(LSTM(hidden_neurons, input_shape=(in_out_neurons, window_size), return_sequences=False))  
+    model.add(Dense(in_out_neurons))  
+    #model.add(Activation("linear"))  
+    model.compile(loss="mean_squared_error", optimizer="rmsprop")  
+    model.fit(data.trainX, data.trainY, batch_size=1, nb_epoch=epoch_count, validation_split=0.05)
+    
+    # make predictions
+    trainPredict = model.predict(data.trainX)
+    testPredict = model.predict(data.testX)
+    
+    # invert predictions
+    #trainPredict = data.scaler.inverse_transform(trainPredict)
+    #data.trainY = data.scaler.inverse_transform([data.trainY])
+    #testPredict = data.scaler.inverse_transform(testPredict)
+    #data.testY = data.scaler.inverse_transform([data.testY])
+    
+    # calculate root mean squared error
+    #trainScore = math.sqrt(mean_squared_error(data.trainY[0,:], trainPredict[:,0]))
+    #print('Train Score: %.2f RMSE' % (trainScore))
+    #testScore = math.sqrt(mean_squared_error(data.testY[0,:], testPredict[:,0]))
+    #print('Test Score: %.2f RMSE' % (testScore))
+    
+    # shift train predictions for plotting
+    trainPredictPlot = np.empty_like(data.frames)
+    trainPredictPlot[:, :] = np.nan
+    trainPredictPlot[window_size:len(trainPredict)+window_size, :] = trainPredict[:,:1]
+    
+    # shift test predictions for plotting
+    testPredictPlot = np.empty_like(data.frames)
+    testPredictPlot[:, :] = np.nan
+    testPredictPlot[len(trainPredict)+(window_size*2)+1:len(data.frames)-1, :] = testPredict[:,:1]
+    
+    # plot baseline and predictions
+    plt.plot(data.scaler.inverse_transform(data.frames))
+    plt.plot(trainPredictPlot)
+    plt.plot(testPredictPlot)
+    plt.show()
+
+def main():
+    example()
+    return 1
+
+if __name__ == "__main__":
+    sys.exit(main())
